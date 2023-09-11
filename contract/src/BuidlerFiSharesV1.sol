f4e2042de4fee60e51ca8d814c9bdb8f2f271985
Yu Shi Jie <yshijie1999@gmail.com>
2023-09-11 15:51:20 +0800

    foundry

--- contract/src/BuidlerFiSharesV1.sol
+++ contract/src/BuidlerFiSharesV1.sol
@@ -1,13 +1,32 @@
-pragma solidity >=0.8.2 <0.9.0;
-import "@openzeppelin/contracts/access/Ownable.sol"
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.19;
+import "openzeppelin/access/Ownable.sol";
+
 
 contract BuidlerFiSharesV1 is Ownable {
+    error FundsTransferFailed();
+    error OnlySharesSubjectCanBuyFirstShare();
+    error CannotSellLastShare();
+    error InsufficientPayment();
+    error InsufficientShares();
+
     address public protocolFeeDestination;
     uint256 public protocolFeePercent;
     uint256 public subjectFeePercent;
     uint256 public hodlerFeePercent;
 
-    event Trade(address trader, address subject, bool isBuy, uint256 shareAmount, uint256 ethAmount, uint256 protocolEthAmount, uint256 subjectEthAmount, uint256 hodlerEthAmount, uint256 supply);
+    event Trade(
+        address trader, 
+        address subject, 
+        bool isBuy, 
+        uint256 shareAmount, 
+        uint256 ethAmount, 
+        uint256 protocolEthAmount,
+        uint256 subjectEthAmount, 
+        uint256 hodlerEthAmount, 
+        uint256 supply
+    );
 
     // SharesSubject => (Hodler => SupporterNumber)
     mapping(address => mapping(address=>uint256)) public supporterNumber;
@@ -42,7 +61,10 @@ contract BuidlerFiSharesV1 is Ownable {
 
     function getPrice(uint256 supply, uint256 amount) public pure returns (uint256) {
         uint256 sum1 = supply == 0 ? 0 : (supply - 1 )* (supply) * (2 * (supply - 1) + 1) / 6;
-        uint256 sum2 = supply == 0 && amount == 1 ? 0 : (supply - 1 + amount) * (supply + amount) * (2 * (supply - 1 + amount) + 1) / 6;
+        uint256 sum2 = supply == 0 && amount == 1 
+        ? 
+        0 : 
+        (supply + amount - 1) * (supply + amount) * (2 * (supply + amount - 1) + 1) / 6;
         uint256 summation = sum2 - sum1;
         return summation * 1 ether / 16000;
     }
@@ -73,16 +95,36 @@ contract BuidlerFiSharesV1 is Ownable {
 
     function buyShares(address sharesSubject, uint256 amount) public payable {
         uint256 supply = sharesSupply[sharesSubject];
-        require(supply > 0 || sharesSubject == msg.sender, "Only the shares' subject can buy the first share");
+        if(supply == 0 && sharesSubject != msg.sender) revert OnlySharesSubjectCanBuyFirstShare();
         uint256 price = getPrice(supply, amount);
         uint256 protocolFee = price * protocolFeePercent / 1 ether;
         uint256 subjectFee = price * subjectFeePercent / 1 ether;
         uint256 hodlerFee = price * hodlerFeePercent / 1 ether;
-        require(msg.value >= price + protocolFee + subjectFee + hodlerFee, "Insufficient payment");
+        if(msg.value < price + protocolFee + subjectFee + hodlerFee) revert InsufficientPayment();
 
-        uint256 balance = sharesBalance[sharesSubject][msg.sender];
+        uint256 initialBalance = sharesBalance[sharesSubject][msg.sender];
+        
+        sharesBalance[sharesSubject][msg.sender] += amount;
+        sharesSupply[sharesSubject] = supply + amount;
+        emit Trade(
+            msg.sender, 
+            sharesSubject, 
+            true, 
+            amount, 
+            price, 
+            protocolFee, 
+            subjectFee, 
+            hodlerFee,
+            supply + amount
+        );
 
-        if (balance==0) {
+        (bool success1, ) = protocolFeeDestination.call{value: protocolFee}("");
+        (bool success2, ) = sharesSubject.call{value: subjectFee}("");
+        bool success3 = _distributeToHodlers(sharesSubject, hodlerFee);
+
+        if(!(success1 && success2 && success3)) revert FundsTransferFailed();
+
+        if (initialBalance==0) {
             supporterNumber[sharesSubject][msg.sender] = supporterKeysArray[sharesSubject].length;
             supporterKeysArray[sharesSubject].push(amount);
             supporterAddressArray[sharesSubject].push(msg.sender);
@@ -91,56 +133,55 @@ contract BuidlerFiSharesV1 is Ownable {
         else {
             supporterKeysArray[sharesSubject][supporterNumber[sharesSubject][msg.sender]] += amount;
         }
-
-        sharesBalance[sharesSubject][msg.sender] += amount;
-        sharesSupply[sharesSubject] = supply + amount;
-        emit Trade(msg.sender, sharesSubject, true, amount, price, protocolFee, subjectFee, supply + amount);
-
-        (bool success1, ) = protocolFeeDestination.call{value: protocolFee}("");
-        (bool success2, ) = sharesSubject.call{value: subjectFee}("");
-        bool success3 = _distributeToHodlers(sharesSubject, hodlerFee);
-        require(success1 && success2 && success3, "Unable to send funds");
     }
 
     function sellShares(address sharesSubject, uint256 amount) public payable {
         uint256 supply = sharesSupply[sharesSubject];
-        require(supply > amount, "Cannot sell the last share");
+        if(supply <= amount) revert CannotSellLastShare();
+        if(sharesBalance[sharesSubject][msg.sender] < amount) revert InsufficientShares();
+
         uint256 price = getPrice(supply - amount, amount);
         uint256 protocolFee = price * protocolFeePercent / 1 ether;
         uint256 subjectFee = price * subjectFeePercent / 1 ether;
         uint256 hodlerFee = price * hodlerFeePercent / 1 ether;
 
-        require(sharesBalance[sharesSubject][msg.sender] >= amount, "Insufficient shares");
-
-        uint256 balance = sharesBalance[sharesSubject][msg.sender];
-
         supporterKeysArray[sharesSubject][supporterNumber[sharesSubject][msg.sender]] -= amount;
         sharesBalance[sharesSubject][msg.sender] -= amount;
         sharesSupply[sharesSubject] = supply - amount;
-        emit Trade(msg.sender, sharesSubject, false, amount, price, protocolFee, subjectFee, supply - amount);
-        (bool success1, ) = msg.sender.call{value: price - protocolFee - subjectFee}("");
+        emit Trade(
+            msg.sender, 
+            sharesSubject, 
+            false, 
+            amount, 
+            price, 
+            protocolFee, 
+            subjectFee, 
+            hodlerFee, 
+            supply - amount
+        );
+        (bool success1, ) = msg.sender.call{value: price - protocolFee - subjectFee - hodlerFee}("");
         (bool success2, ) = protocolFeeDestination.call{value: protocolFee}("");
         (bool success3, ) = sharesSubject.call{value: subjectFee}("");
         bool success4 = _distributeToHodlers(sharesSubject, hodlerFee);
 
-        require(success1 && success2 && success3 && success4, "Unable to send funds");
+        if(!(success1 && success2 && success3 && success4)) revert FundsTransferFailed();
     }
 
-    function _distributeToHodlers(address sharesSubject, hodlerFee) internal payable returns(bool) {
+    function _distributeToHodlers(address sharesSubject, uint256 hodlerFee) internal returns(bool) {
         (address[] memory addresses, uint256 number) = getAtMostTopTenSupporters(sharesSubject);
 
         // Edge case: somehow can't find a single hodler within 1000 range
         if(number==0) {
             (bool success, ) = protocolFeeDestination.call{value: hodlerFee}("");
             
-            if(!success) revert();
+            if(!success) revert FundsTransferFailed();
         }
 
         else {
             uint256 feePerHodler = hodlerFee * 1e18 / number / 1e18;
-            for(uint256 i=0; i<numbers;){
+            for(uint256 i=0; i<number;){
                 (bool success, ) = addresses[i].call{value:feePerHodler}("");
-                if(!success) revert();
+                if(!success) revert FundsTransferFailed();
 
                 unchecked {
                     ++i;
@@ -153,13 +194,14 @@ contract BuidlerFiSharesV1 is Ownable {
     function getAtMostTopTenSupporters(address sharesSubject) public view returns(address[] memory,uint256) {
         address[] memory addresses = new address[](10);
         uint256 counter = 0;
-        for(uint256 i=0;i<supporterKeysArray[sharesSubject];) {
-            if(supporterKeysArray[i]!=0) {
-                addresses[counter] = supporterAddressArray[i];
+        uint256 length = supporterKeysArray[sharesSubject].length;
+        for(uint256 i=0;i<length;) {
+            if(supporterKeysArray[sharesSubject][i]!=0) {
+                addresses[counter] = supporterAddressArray[sharesSubject][i];
                 counter++;
             }
 
-            if(counter > 10 || i > 999) {
+            if(counter > 9 || i > 999) {
                 break;
             }
 
---
+++ contract/test/BuidlerFiSharesV1.t.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.19 <0.9.0;
+
+import { console2 } from "forge-std/console2.sol";
+import { StdCheats } from "forge-std/StdCheats.sol";
+import "forge-std/Test.sol";
+import "forge-std/Vm.sol";
+
+import { BuidlerFiSharesV1 } from "../src/BuidlerFiSharesV1.sol";
+
+/// @dev If this is your first time with Forge, read this tutorial in the Foundry Book:
+/// https://book.getfoundry.sh/forge/writing-tests
+contract BuidlerFiSharesV1Test is StdCheats, Test {
+    BuidlerFiSharesV1 public sharesContract;
+
+    /// @dev A function invoked before each test case is run.
+    function setUp() public virtual {
+        // Instantiate the contract-under-test.
+        sharesContract = new BuidlerFiSharesV1();
+        sharesContract.setFeeDestination(address(0x99));
+        sharesContract.setHodlerFeePercent(5e16); // 5%
+        sharesContract.setProtocolFeePercent(5e16); // 5%
+        sharesContract.setSubjectFeePercent(5e16); // 5%
+    }
+
+    function test_InitialState() external {
+        assertEq(sharesContract.protocolFeeDestination(), address(0x99), "value mismatch");
+        assertEq(sharesContract.hodlerFeePercent(), 5e16, "value mismatch");
+        assertEq(sharesContract.protocolFeePercent(), 5e16, "value mismatch");
+        assertEq(sharesContract.subjectFeePercent(), 5e16, "value mismatch");
+
+    }
+
+    function test_SuccessfulInitialBuySingleKey() external {
+        vm.startPrank(address(0x1));
+        uint256 buyPrice = sharesContract.getBuyPriceAfterFee(address(0x1),1);
+        vm.deal(address(0x1), buyPrice);
+        sharesContract.buyShares{value: 0}(address(0x1),1);
+        assertEq(sharesContract.supporterAddressArray(address(0x1),0), address(0x1), "Array not updated");
+        vm.stopPrank();
+    }
+
+    function test_SuccessfulInitialBuyMultipleKeys() external {
+        vm.startPrank(address(0x1));
+        uint256 buyPrice = sharesContract.getBuyPriceAfterFee(address(0x1),5);
+        vm.deal(address(0x1), buyPrice);
+        sharesContract.buyShares{value: buyPrice}(address(0x1),5);
+        assertEq(sharesContract.supporterAddressArray(address(0x1),0), address(0x1), "Array not updated");
+        vm.stopPrank();
+    }
+
+    function test_UnsuccessfulInitialBuy_FirstShareByNonOwner() external {
+        vm.startPrank(address(0x1));
+        uint256 buyPrice = sharesContract.getBuyPriceAfterFee(address(0x1),1);
+        vm.deal(address(0x1), buyPrice);
+        vm.expectRevert();
+        sharesContract.buyShares{value: buyPrice}(address(0x2),1);
+        vm.stopPrank();
+    }
+
+    function test_SupporterNumber() external {
+        vm.startPrank(address(0x1));
+        uint256 buyPrice1 = sharesContract.getBuyPriceAfterFee(address(0x1),1);
+        vm.deal(address(0x1), buyPrice1);
+        sharesContract.buyShares{value:buyPrice1}(address(0x1),1);
+        vm.stopPrank();
+        vm.startPrank(address(0x2));
+        uint256 buyPrice2 = sharesContract.getBuyPriceAfterFee(address(0x1),2);
+        vm.deal(address(0x2), buyPrice2);
+        sharesContract.buyShares{value:buyPrice2}(address(0x1),2);
+        vm.stopPrank();
+        vm.startPrank(address(0x3));
+        uint256 buyPrice3 = sharesContract.getBuyPriceAfterFee(address(0x1),3);
+        vm.deal(address(0x3), buyPrice3);
+        sharesContract.buyShares{value:buyPrice3}(address(0x1),3);
+        vm.stopPrank();
+        assertEq(sharesContract.supporterAddressArray(address(0x1),0), address(0x1), "Array not updated");
+        assertEq(sharesContract.supporterAddressArray(address(0x1),1), address(0x2), "Array not updated");
+        assertEq(sharesContract.supporterAddressArray(address(0x1),2), address(0x3), "Array not updated");
+
+        assertEq(sharesContract.supporterKeysArray(address(0x1),0), 1, "Array not updated");
+        assertEq(sharesContract.supporterKeysArray(address(0x1),1), 2, "Array not updated");
+        assertEq(sharesContract.supporterKeysArray(address(0x1),2), 3, "Array not updated");
+    }
+
+    function test_getTopNHodlers() external {
+        for(uint256 i=1;i<10;i++) {
+            vm.startPrank(address(uint160(i)));
+            uint256 buyPrice = sharesContract.getBuyPriceAfterFee(address(0x1),1);
+            vm.deal(address(uint160(i)), buyPrice);
+            sharesContract.buyShares{value:buyPrice}(address(0x1),1);
+            vm.stopPrank();
+        }
+
+        (address[] memory addresses,uint256 number) = sharesContract.getAtMostTopTenSupporters(address(0x1));
+        assertEq(addresses[0], address(0x1), "Invalid address");
+        assertEq(addresses[4], address(0x5), "Invalid address");
+        assertEq(addresses[8], address(uint160(9)), "Invalid address");
+        assertEq(number, 9, "Invalid N");
+    }
+}
