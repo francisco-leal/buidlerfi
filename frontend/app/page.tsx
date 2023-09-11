24fea667d78043655daae71f2c505d8595a394d3
francisco-leal <francisco@talentprotocol.com>
2023-09-11 10:40:40 +0100

    Update explore page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,6 +4,8 @@ import { DATA, DEFAULT_PROFILE_PICTURE } from '@/lib/mock';
 import { Button } from '@/components/ui/button';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { useRouter } from 'next/navigation'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { ChevronRight } from "lucide-react"
 
 export default function Home() {
   const router = useRouter();
@@ -23,26 +25,62 @@ export default function Home() {
   if (isConnecting) return <h1 className="text-lg font-large leading-none p-8">Connecting...</h1>
   if (isDisconnected) return <h1 className="text-lg font-large leading-none p-8">Please connect your wallet to proceed</h1>
 
+  const sortUsers = (a: { number_of_holders: number }, b: { number_of_holders: number }) => (a.number_of_holders > b.number_of_holders) ? -1 : 1;
+
   return (
     <main className="py-4 px-2">
-      <h2 className="text-lg font-medium leading-none mb-4 ml-2">Recommended</h2>
-      {DATA.map((item) => 
-        <div key={`home-${item.wallet}`} className="flex items-center justify-between w-full rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground">
-          <div className="space-x-4 flex items-center">
-            <Avatar className="mt-px h-5 w-5">
-              <AvatarImage src={item.profile_picture_url || DEFAULT_PROFILE_PICTURE} />
-              <AvatarFallback>OM</AvatarFallback>
-            </Avatar>
-            <div className="space-y-1">
-              <p className="text-sm font-medium leading-none">{builderName(item)}</p>
-              <p className="text-sm text-muted-foreground">
-                {item.number_of_holders} holders | {item.number_of_votes} Votes
-              </p>
+      <Tabs defaultValue="recommended" className="space-y-4">
+        <TabsList className="grid w-full grid-cols-2 mb-8">
+          <TabsTrigger value="top">
+            Top
+          </TabsTrigger>
+          <TabsTrigger value="recommended">
+            Recommended
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="top" className="space-y-4">
+          {DATA.sort(sortUsers).map((item) => 
+            <div key={`home-${item.wallet}`} className="flex items-center justify-between w-full rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground" onClick={() => router.push(`/${item.wallet}`)}>
+              <div className="space-x-4 flex items-center">
+                <Avatar className="mt-px h-5 w-5">
+                  <AvatarImage src={item.profile_picture_url || DEFAULT_PROFILE_PICTURE} />
+                  <AvatarFallback>OM</AvatarFallback>
+                </Avatar>
+                <div className="space-y-1">
+                  <p className="text-sm font-medium leading-none">{builderName(item)}</p>
+                  <p className="text-sm text-muted-foreground">
+                    {item.number_of_holders} holders | Price {price(item)} MATIC
+                  </p>
+                </div>
+              </div>
+              <Button variant="ghost" size="icon" onClick={() => router.push(`/${item.wallet}`)}>
+                <ChevronRight className="h-4 w-4" />
+              </Button>
             </div>
-          </div>
-          <Button onClick={() => router.push(`/${item.wallet}`)}>Buy {price(item)} ETH</Button>
-        </div>
-      )}
+          )}
+        </TabsContent>
+        <TabsContent value="recommended" className="space-y-4">
+          {DATA.sort(sortUsers).map((item) => 
+            <div key={`home-${item.wallet}`} className="flex items-center justify-between w-full rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground">
+              <div className="space-x-4 flex items-center">
+                <Avatar className="mt-px h-5 w-5">
+                  <AvatarImage src={item.profile_picture_url || DEFAULT_PROFILE_PICTURE} />
+                  <AvatarFallback>OM</AvatarFallback>
+                </Avatar>
+                <div className="space-y-1">
+                  <p className="text-sm font-medium leading-none">{builderName(item)}</p>
+                  <p className="text-sm text-muted-foreground">
+                    {item.number_of_holders} holders | Price {price(item)} MATIC
+                  </p>
+                </div>
+              </div>
+              <Button variant="ghost" size="icon" onClick={() => router.push(`/${item.wallet}`)}>
+                <ChevronRight className="h-4 w-4" />
+              </Button>
+            </div>
+          )}
+        </TabsContent>
+      </Tabs>
     </main>
   )
 }
