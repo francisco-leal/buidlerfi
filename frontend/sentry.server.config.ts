1f8178251e527dc801beeea579cc8d2aa98ebaf0
francisco-leal <francisco@talentprotocol.com>
2023-11-08 16:05:43 +0000

    Add sentry config

---
+++ frontend/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://3e2757dd1176aaa195269d084220c6c8@o4506190650081280.ingest.sentry.io/4506190651129856",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
