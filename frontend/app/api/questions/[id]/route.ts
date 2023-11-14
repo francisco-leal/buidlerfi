e2aff981dd5853ac6197d402f2a1c7e8c114349e
francisco-leal <francisco@talentprotocol.com>
2023-11-14 12:35:35 +0300

    Check if the replier is the owner of the profile

--- frontend/app/api/questions/[id]/route.ts
+++ frontend/app/api/questions/[id]/route.ts
@@ -4,11 +4,20 @@ import prisma from "@/lib/prisma";
 export async function PUT(req: Request, { params }: { params: { id: number } }) {
   try {
     const body = await req.json();
-    const question = await prisma.question.findUnique({ where: { id: params.id } });
+    const question = await prisma.question.findUnique({ where: { id: Number(params.id) } });
     if (!question) return Response.json({ error: ERRORS.QUESTION_NOT_FOUND }, { status: 404 });
-    //TODO Check if question replier is current user
 
-    const res = await prisma.question.update({ where: { id: params.id }, data: body });
+    const replier = await prisma.user.findUnique({ where: { privyUserId: req.headers.get("privyUserId")! } });
+
+    if (question.replierId !== replier?.id) return Response.json({ error: ERRORS.UNAUTHORIZED }, { status: 401 });
+
+    const res = await prisma.question.update({
+      where: { id: Number(params.id) },
+      data: {
+        reply: body["answerContent"],
+        repliedOn: new Date()
+      }
+    });
 
     return Response.json({ data: res }, { status: 200 });
   } catch (error) {
