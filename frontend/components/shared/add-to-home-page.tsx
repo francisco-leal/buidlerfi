ec42abbfc1dafca868d2e21624cfb96334ee808a
Francisco Leal <francisco@talentprotocol.com>
2023-11-23 11:18:55 +0000

    Homescreen (#96)
    * Add homescreen state to ask to add to home screen
    
    * Add to homescreen button on menu
    
    * Add sociald ata to recomended users table
    
    * Fix quotes in text

--- frontend/components/shared/add-to-home-page.tsx
+++ frontend/components/shared/add-to-home-page.tsx
@@ -1,56 +1,59 @@
-import { AddToHomeScreen } from "@mui/icons-material";
-import { Button } from "@mui/joy";
+import { Flex } from "@/components/shared/flex";
+import { AddToHomeScreen, Close, IosShare } from "@mui/icons-material";
+import { Button, DialogTitle, IconButton, Modal, ModalDialog, Typography } from "@mui/joy";
 import { useEffect, useState } from "react";
 
-type InstallEvent = {
-  prompt?: () => void;
-  userChoice?: Promise<{ outcome: string }>;
-  preventDefault: () => void;
-};
+interface Navigator extends globalThis.Navigator {
+  standalone?: boolean;
+}
 
 export function AddToHomePage() {
-  const [installEvent, setInstallEvent] = useState<InstallEvent | null>(null);
-
+  const [instructionsOpen, setInstructionsOpen] = useState(false);
+  const [isInstalled, setIsInstalled] = useState(false);
   useEffect(() => {
-    const beforeInstallPromptHandler = (e: InstallEvent) => {
-      if (e) {
-        e.preventDefault();
-        setInstallEvent(e);
-      }
-    };
-
-    window.addEventListener("beforeinstallprompt", beforeInstallPromptHandler);
-
-    return () => {
-      return window.removeEventListener("beforeinstallprompt", beforeInstallPromptHandler);
-    };
+    const isStandalone = window.matchMedia("(display-mode: standalone)").matches;
+    const { standalone } = navigator as Navigator;
+    if (document.referrer.startsWith("android-app://")) {
+      setIsInstalled(true);
+    } else if (standalone || isStandalone) {
+      setIsInstalled(true);
+    }
+    setIsInstalled(false);
   }, []);
 
-  const handleInstallClick = () => {
-    if (!installEvent) return;
-
-    if (installEvent.prompt) {
-      installEvent?.prompt();
-    }
-    if (!installEvent.userChoice) return;
-
-    installEvent.userChoice.then(choiceResult => {
-      if (choiceResult.outcome === "accepted") {
-        console.log("User accepted the install prompt");
-      } else {
-        console.log("User dismissed the install prompt");
-      }
-    });
-  };
-
-  if (!installEvent) {
-    return null;
-  }
+  if (isInstalled) return null;
 
   return (
-    <Button onClick={handleInstallClick} disabled={!!installEvent}>
-      <AddToHomeScreen />
-      Add to Home Screen
-    </Button>
+    <>
+      <Button onClick={() => setInstructionsOpen(true)}>
+        <AddToHomeScreen />
+        Add to Home Screen
+      </Button>
+      <Modal open={instructionsOpen} onClose={() => setInstructionsOpen(true)}>
+        <ModalDialog minWidth="400px">
+          <Flex x xsb yc>
+            <DialogTitle>Add to Home Screen</DialogTitle>
+            <IconButton onClick={() => setInstructionsOpen(true)}>
+              <Close />
+            </IconButton>
+          </Flex>
+          <Typography level="body-sm" textColor="neutral.500">
+            Add builder.fi to your home screen to get the best experience
+          </Typography>
+          <Flex x yc gap={2}>
+            <Typography level="body-sm" textColor="neutral.500">
+              1. Click the share button
+            </Typography>
+            <IosShare />
+          </Flex>
+          <Typography level="body-sm" textColor="neutral.500">
+            2. Scroll down
+          </Typography>
+          <Typography level="body-sm" textColor="neutral.500">
+            3. Click &quot;Add to Home Screen&quot;
+          </Typography>
+        </ModalDialog>
+      </Modal>
+    </>
   );
 }
---
+++ frontend/prisma/migrations/20231123110600_add_social_data_to_recommended_users/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "RecommendedUser" ADD COLUMN     "ens" TEXT,
+ADD COLUMN     "farcaster" TEXT,
+ADD COLUMN     "lens" TEXT,
+ADD COLUMN     "talentProtocol" TEXT;
